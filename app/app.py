58e902a94e9652d44e247285e553a7de59f5dd73
Vineeth_Gujja <vineethuchiha@gmail.com>
2025-09-05 17:27:15 +0000

    added my picture

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ def home():
     return render_template_string("""
         <h1>Hello from Flask App 🚀</h1>
         <p>This is an updated version with an image!</p>
-        <img src="{{ url_for('static', filename='DP.jpeg') }}" alt="Flask Logo" width="200">
+        <img src="{{ url_for('static', filename='dp.png') }}" alt="Flask Logo" width="200">
     """)
 
 if __name__ == "__main__":
