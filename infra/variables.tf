d0d3e1668639607df25a1435de4890d524e80d0b
Vineeth_Gujja <vineethuchiha@gmail.com>
2025-09-10 19:36:53 +0000

    made small changes to infra

---
+++ infra/variables.tf
@@ -0,0 +1,46 @@
+variable "project_name" {
+	description = "Name prefix for all resources"
+	type = string
+	default = "autoscale-flask"
+}
+
+variable "region" {
+	description = "AWS region"
+	type = string
+	default = "ap-south-1"
+}
+
+variable "vpc_cidr"{
+	description = "CIDR for VPC"
+	type = string
+	default = "10.0.0.0/16"
+}
+
+variable "azs" {
+	description = "Availability zones"
+	type = list(string)
+	default = ["ap-south-1a", "ap-south-1b"]
+}
+
+variable "public_subnets" {
+	description = "Public subnet CIDRs"
+  	type        = list(string)
+  	default     = ["10.0.0.0/24"]
+}
+variable "cluster_version" {
+  description = "EKS Kubernetes version"
+  type        = string
+  default     = "1.29"
+}
+
+variable "desired_capacity" {
+  description = "Desired node count for the default node group"
+  type        = number
+  default     = 2
+}
+
+variable "instance_types" {
+  description = "Instance types for node group"
+  type        = list(string)
+  default     = ["t3.small"]
+}
