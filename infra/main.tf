5c46b8f9a7184a1766fd2d7b577bb2d801691dcf
Vineeth_Gujja <vineethuchiha@gmail.com>
2025-09-16 17:38:15 +0000

    Created k8 cluster

--- infra/main.tf
+++ infra/main.tf
@@ -1,15 +1,18 @@
+
 # VPC
 
 module "vpc" {
-
+  
 	source = "terraform-aws-modules/vpc/aws"
-	version = "5.5.1"
+	version = "5.8.1"
 	
 	name = "${var.project_name}-vpc"
 	azs = var.azs
 	public_subnets = var.public_subnets
+	private_subnets = var.private_subnets
 
-	enable_nat_gateway = false
+	enable_nat_gateway = true
+	single_nat_gateway = true
 
 	tags = {
 	    Project = var.project_name
@@ -20,24 +23,26 @@ module "vpc" {
 
 module "eks" {
     source = "terraform-aws-modules/eks/aws"
-    version = "20.24.0"
+    version = "20.8.5"
       
     cluster_name = "${var.project_name}-eks"
     cluster_version = var.cluster_version
 
     cluster_endpoint_public_access = true
+    
+    vpc_id = module.vpc.vpc_id
+    subnet_ids = module.vpc.private_subnets
 
     enable_irsa = false
 
     eks_managed_node_groups = {
 	default = {
+	    instance_types = var.instance_types
+	    desired_size = var.desired_capacity
 	    min_size = 1
 	    max_size = 3
-	    desired_size = var.desired_capacity
-	    instance_types = t3.micro
-	    disk_size = 10
 	}
-}
+   }
 
      tags = {
 	Project = var.project_name
--- infra/variables.tf
+++ infra/variables.tf
@@ -25,8 +25,14 @@ variable "azs" {
 variable "public_subnets" {
 	description = "Public subnet CIDRs"
   	type        = list(string)
-  	default     = ["10.0.0.0/24"]
+  	default     = ["10.0.0.0/24" , "10.0.1.0/24"]
 }
+variable "private_subnets" {
+        description = "Private subnet CIDRs"
+        type        = list(string)
+        default     = ["10.0.2.0/24" , "10.0.3.0/24"]
+}
+
 variable "cluster_version" {
   description = "EKS Kubernetes version"
   type        = string
